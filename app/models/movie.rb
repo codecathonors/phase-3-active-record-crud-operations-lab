902681750cb3895fa7cb92ab59f71a45f3725533
CatherineHonors <cathonors@gmail.com>
2022-05-20 16:30:03 -0400

    commit tree be3ca8773a6ed17f7d1d6a6901f9ae1eec7a3605

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,34 @@
-class Movie < ActiveRecord::Base
 
+
+class Movie < ActiveRecord::Base
+    attr_accessor :title
+    
+    def self.create_with_title title
+        self.create(title: title)
+    end
+
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        self.find_by(attributes)
+    end
+
+    def self.find_movies_after_2002
+        self.where("release_date > 2002")
+    end
 end
+
